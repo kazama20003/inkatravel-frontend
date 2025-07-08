7a1651a4e4a69513cc8d3b61de5ef8fcb67bcfed
kazama20003 <kazamatower@gmail.com>
2025-07-08 15:01:00 -0500

    /ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -158,7 +158,7 @@ export default function RootLayout({
                 "Agencia de turismo líder en Perú especializada en tours a Cusco, Cabanaconde, Arequipa y Machu Picchu",
               url: "https://cabanacondecuscobybus.com",
               logo: "https://cabanacondecuscobybus.com/logo.png",
-              image: "https://cabanacondecuscobybus.com/screenshot/principal.png",
+              image: "https://cabanacondecuscobybus.com/screenshot/image.png",
               telephone: "+51-996-407-040",
               email: "incatravelperu21@gmail.com",
               address: {
