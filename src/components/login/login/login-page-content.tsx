b40513fa90f0f4d4c8eb1b3940971b9b8a31be77
kazama20003 <kazamatower@gmail.com>
2025-11-21 02:33:53 -0500

    /cart

--- src/components/login/login/login-page-content.tsx
+++ src/components/login/login/login-page-content.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React from "react"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 import { toast } from "sonner"
 import { api } from "@/lib/axiosInstance"
 
