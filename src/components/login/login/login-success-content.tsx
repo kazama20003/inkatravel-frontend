54266bfecdaab7b69b058c6487aae99817e9ecd8
kazama20003 <kazamatower@gmail.com>
2025-11-21 02:29:25 -0500

    /cart

--- src/components/login/login/login-success-content.tsx
+++ src/components/login/login/login-success-content.tsx
@@ -2,7 +2,7 @@
 
 import React from "react"
 import { useEffect } from "react"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 import { useSearchParams } from "next/navigation"
 import { toast } from "sonner"
 
